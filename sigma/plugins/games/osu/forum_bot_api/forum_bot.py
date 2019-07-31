2a01dd37acc466b02091fab365433f2e1535d076
abraker <abraker95@gmail.com>
2019-07-30 22:59:28 -0400

    Integrate new key field as osu user id

--- sigma/plugins/games/osu/forum_bot_api/forum_bot.py
+++ sigma/plugins/games/osu/forum_bot_api/forum_bot.py
@@ -1,8 +1,7 @@
-import discord
-import aiohttp
-
 from sigma.core.ctrl_client import CtrlClient
 from .forum_bot_handler import handle_data
+from ..misc.profile_helper import get_osu_profile
+
 
 
 async def forum_bot(cmd, message, args):
@@ -14,16 +13,19 @@ async def forum_bot(cmd, message, args):
     try:    bot, name = args[0].split('.')
     except: bot, name = 'Core', 'help'
 
+    osu_profile = get_osu_profile(cmd, message.author)
+    if osu_profile == None: osu_id = None
+    else:                   osu_id = osu_profile['osu_id']
+
     data = {
         'bot'  : bot,
         'cmd'  : name,
-        'args' : []
+        'args' : [],
+        'key'  : osu_id
     }
 
     if len(args) > 1: 
         data['args'] = args[1:]
 
-
     success = await CtrlClient(cmd.log, 44444, handle_data).request(data, (message, cmd.log))
-    if not success:
-        await message.channel.send('Failed')
+    if not success: await message.channel.send('Failed')
